cb294ecb27dff2b16c4b716734d27d33ca196d17
Jeneara <mail@jeneara.com>
2021-12-06 14:33:51 +1100

    tiny slider

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
-
-
 // Scrollspy
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#navbarMain',
     offset: 100,
-  })
+})
+
+// Owl Carousel
+var slider = tns({
+    container: '.my-slider',
+    items: 3,
+    slideBy: 'page',
+    autoplay: true
+  });
