50cd9111548d9e6bcb2faab2fed280849a64340a
Jeneara <mail@jeneara.com>
2021-12-10 11:12:29 +1100

    caro update

--- scripts.js
+++ scripts.js
@@ -1,33 +1,36 @@
 // Tiny Slider
-let slider = tns({
-    container: '.my-slider',
-    items: 3,
-    autoplay: true,
-    mouseDrag: true,
-    swipeAngle: false,
-    speed: 400,
-    touch: true,
-    autoheight: true,
-    controlsPosition: "bottom",
-    navAsThumbnails: true,
-    // edgePadding: 50,
-    // fixedWidth: 500,
-    responsive: {
-        340: {
-            edgePadding: 20,
-            gutter: 20,
-            items: 1
-        },
-        700: {
-            edgePadding: 20,
-            gutter: 30,
-            items: 1
-        },
-        900: {
-            items: 1
+document.querySelectorAll('.my-slider').forEach(slider => {
+    tns({
+        container: slider,
+        items: 3,
+        autoplay: false,
+        mouseDrag: true,
+        swipeAngle: false,
+        speed: 1000,
+        touch: true,
+        autoheight: true,
+        controlsPosition: "bottom",
+        navAsThumbnails: true,
+        // edgePadding: 50,
+        // fixedWidth: 500,
+        responsive: {
+            340: {
+                edgePadding: 20,
+                gutter: 20,
+                items: 1
+            },
+            700: {
+                edgePadding: 20,
+                gutter: 30,
+                items: 1
+            },
+            900: {
+                items: 1
+            }
         }
-    }
+    });
 });
+
 
 // Scrollspy
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
