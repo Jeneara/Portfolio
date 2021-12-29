1b2088cad67013c11050fffb62720aaf8f4c61c3
Jeneara <mail@jeneara.com>
2021-12-29 23:10:49 +1100

    update

--- scripts.js
+++ scripts.js
@@ -3,11 +3,3 @@ var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#navbarMain',
     offset: 100,
 })
-
-//Close
-const navLinks = document.querySelectorAll('.nav-link')
-const menuToggle = document.getElementById('navbarNav')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
