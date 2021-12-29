56f3f4dd8b7277f93741a36683dd3a47552968bb
Jeneara <mail@jeneara.com>
2021-12-29 22:31:04 +1100

    update

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-
-
-
 // Scrollspy
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#navbarMain',
     offset: 100,
 })
 
-
-
-
-
+//Close
+const navLinks = document.querySelectorAll('.nav-link')
+const menuToggle = document.getElementById('navbarNav')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
