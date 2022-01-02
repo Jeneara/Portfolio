e0cf7f626047bd9f45d7b81a8f8bd06d2845a59e
Jeneara <mail@jeneara.com>
2022-01-02 19:18:11 +1100

    update

--- scripts.js
+++ scripts.js
@@ -1,8 +1,75 @@
-// Scrollspy
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbar',
-    offset: 100,
+// Theme Switch
+
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
+btn.addEventListener("click", function () {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute("href") == "light-theme.css") {
+    // ... then switch it to "dark-theme.css"
+    theme.href = "dark-theme.css";
+    // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = "light-theme.css";
+  }
 });
 
-//Navbar Close
 
+
+
+//Contact Form
+$(function () {
+
+  // init the validator
+  // validator files are included in the download package
+  // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+  $('#contact-form').validator();
+
+
+  // when the form is submitted
+  $('#contact-form').on('submit', function (e) {
+
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+      var url = "contact.php";
+
+      // POST values in the background the the script URL
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $(this).serialize(),
+        success: function (data) {
+          // data = JSON object that contact.php returns
+
+          // we recieve the type of the message: success x danger and apply it to the 
+          var messageAlert = 'alert-' + data.type;
+          var messageText = data.message;
+
+          // let's compose Bootstrap alert box HTML
+          var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+          // If we have messageAlert and messageText
+          if (messageAlert && messageText) {
+            // inject the alert to .messages div in our form
+            $('#contact-form').find('.messages').html(alertBox);
+            // empty the form
+            $('#contact-form')[0].reset();
+          }
+        }
+      });
+      return false;
+    }
+  })
+});
+
+//Navbar hamburger Close on click
+$(window).resize(function () {
+  if ($(window).width() < 960) {
+    alert('Less than 960');
+  }
+});
